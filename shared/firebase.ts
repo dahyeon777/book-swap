1628546e8533904c450c910ec94dd3f9a7a0ed34
dahyeon777 <daaaa0727@gmail.com>
2025-12-20 16:50:35 +0900

    인증 확인용 커밋

--- shared/firebase.ts
+++ shared/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+//인증
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
